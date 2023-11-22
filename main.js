aad2b491246f7292b7a02bac8b19f551e41bb5ac
lumiuko <kudavidss@gmail.com>
2023-11-23 03:13:18 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const cardValues = document.querySelectorAll('.card-value')
+
+const date = new Date()
+const targetDate = new Date(
+  date.getFullYear(),
+  date.getMonth(),
+  date.getDate() + 15,
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+  date.getMilliseconds()
+)
+
+function getTimeDifference(start, end) {
+  let difference = end - start
+
+  const days = Math.floor(difference / (24 * 60 * 60 * 1000))
+  difference %= 24 * 60 * 60 * 1000
+
+  const hours = Math.floor(difference / (60 * 60 * 1000))
+  difference %= 60 * 60 * 1000
+
+  const minutes = Math.floor(difference / (60 * 1000))
+  difference %= 60 * 1000
+
+  const seconds = Math.floor(difference / 1000)
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+  }
+}
+
+function calculateDate() {
+  const now = new Date()
+  const timeDifference = getTimeDifference(now, targetDate)
+
+  const isInPast = Object.values(timeDifference).every(value => value < 0)
+  if (isInPast) return
+
+  cardValues.forEach(element => {
+    element.textContent = String(timeDifference[element.id] ?? 0).padStart(2, '0')
+  })
+}
+
+calculateDate()
+setInterval(calculateDate, 1000)
